9f16d8c336e02f2c69f5cba65ff174f74d13e61e
= <1035271660@qq.com>
2018-09-12 23:51:41 +0800

    Initial commit

---
+++ routes/shops.js
@@ -0,0 +1,40 @@
+const GROUP_NAME = 'shops';
+const Joi = require('joi');
+
+module.exports = [
+    {
+        method: 'GET',
+        path: `/${GROUP_NAME}`,
+        handler: async (request, reply) => {
+            reply();
+        },
+        config: {
+            tags: ['api', `${GROUP_NAME}`],
+            description: '获取商店列表接口',
+            validate: {
+                query: {
+                    limit: Joi.number().integer().min(1).default(10)
+                        .description('每页条目数'),
+                    page: Joi.number().integer().min(1).default(1)
+                        .description('当前页码'),
+                },
+            },
+        },
+    },
+    {
+        method: 'GET',
+        path: `/${GROUP_NAME}/{shopId}/goods`,
+        handler: async (request, reply) => {
+            reply();
+        },
+        config: {
+            tags: ['api', GROUP_NAME],
+            description: '获取店铺的商品列表',
+            validate: {
+                params: {
+                    shopId: Joi.string().required(),
+                },
+            },
+        },
+    },
+];
