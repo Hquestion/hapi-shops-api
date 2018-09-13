07f6ac18c1c432d7d2918d36347596254e28c0c7
贺小雷 <xiaolei.he@iluvatar.ai>
2018-09-13 18:10:36 +0800

    数据库migrate

--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
+const env2 = require('env2');
+
 if (process.env.NODE_ENV === 'production') {
-    require('env2')('./.env.prod');
+    env2('./.env.prod');
 } else {
-    require('env2')('./.env');
+    env2('./.env');
 }
 const { env } = process;
 
